a9c46200e49711bc01003de2bbaa5702d255fcbd
0Yasser <yasiirh@gmail.com>
2021-11-21 13:04:43 +0300

    completing functionalities: adding and deleting new comments

--- w04d05hw/src/App.js
+++ w04d05hw/src/App.js
@@ -3,7 +3,7 @@ import Post from './components/Post';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import About from './components/About';
-import Comment from './components/Weather';
+import Weather from './components/Weather';
 
 function App() {
   return (
@@ -12,14 +12,17 @@ function App() {
         <Navbar />
         <header className="App-header">
           <Switch>
+            <Route exact path="/">
+              <Post />
+              </Route>
             <Route path="/Post">
               <Post />
             </Route>
-            <Route exact path="/about">
+            <Route  path="/About">
               <About />
             </Route>
             <Route path="/Weather">
-              <Comment />
+              <Weather />
             </Route>
           </Switch>
         </header>
--- w04d05hw/src/components/Comment.js
+++ w04d05hw/src/components/Comment.js
@@ -1,10 +1,39 @@
 import React, { useState } from "react";
 
 function Comment(props) {
-    const [comments,setComments]=useState([['Welcome','Hi'],['First comment','first second comment']])
-    return (
-        <div className="comments">{comments[props.id]==undefined?"":comments[props.id].map((e)=>(<h6 className="comment">{e}</h6>))}</div>
-     );
+  const [comments, setComments] = useState([]);
+  const [NEW, setNEW] = useState("");
+
+  function remove(num){
+    let commentsClone = [...comments];
+    switch (commentsClone.length) {
+        case 1:
+        case 0:
+            setComments([]);
+            break;
+        default:
+            commentsClone.splice(num,1);
+            setComments(commentsClone);
+            break;
+    }
+    
+  }
+  
+  return (
+    <div className="comments">
+      {comments.map((e,i) => (
+          <div key={i}>
+            <button onClick={()=>remove(i)}>delete</button>
+            <h6 className="comment">{e}</h6>
+          </div>
+        ))
+      }
+      <div>
+        <input onChange={(e) => setNEW(e.target.value)} />
+        <button onClick={() => setComments([...comments,NEW])}>Add</button>
+      </div>
+    </div>
+  );
 }
 
 export default Comment;
